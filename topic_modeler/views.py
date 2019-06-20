139f21f9ab0521ffe2a1efb37a1342784d2d27f9
Alexander Suvorov <alexander.suvorov@agriculture.gov.ie>
2019-06-20 11:15:25 +0100

    simple branch

--- topic_modeler/views.py
+++ topic_modeler/views.py
@@ -4,7 +4,6 @@ from rest_framework import viewsets
 from topic_modeler.models import TopicModel, TopicExtractionJob, Topic, TopicWord, TrainData, DataRaw, RunningTasks
 from topic_modeler.serializers import TopicModelSerializer, TopicSerializer, TopicWordSerializer, \
     TopicExtractionJobSerializer, TrainDataSerializer, DataRawSerializer, RunningTasksSerializer
-from topic_modeler.topic_modeler import schedule_train_topic_model
 
 
 class RunningTasksViewSet(viewsets.ReadOnlyModelViewSet):
@@ -40,20 +39,6 @@ class TopicWordViewSet(viewsets.ReadOnlyModelViewSet):
 class TopicExtractionJobViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = TopicExtractionJob.objects.all().order_by('-id')
     serializer_class = TopicExtractionJobSerializer
-
-
-def train_model(request):
-    # extract parameters
-    number_of_topics = request.GET['number_of_topics']
-    words_per_topic = request.GET['words_per_topic']
-    # schedule job
-    message = schedule_train_topic_model(number_of_topics, words_per_topic)
-    # done
-    return HttpResponse(message)
-
-
-def extract_topic(request):
-    pass
 
 
 def ping_topic_modeler(request):
