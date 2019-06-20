4946673e2b862a1ac8fb18b9dd037572fc53bdcf
Alexander Suvorov <alexander.suvorov@agriculture.gov.ie>
2019-06-20 13:54:47 +0100

    rest simple

--- project/database.py
+++ project/database.py
@@ -5,31 +5,25 @@ from django.conf import settings
 
 engines = {
     'sqlite': 'django.db.backends.sqlite3',
-    'postgresql': 'django.db.backends.postgresql_psycopg2'
+    'postgresql': 'django.db.backends.postgresql_psycopg2',
+    'mysql': 'django.db.backends.mysql',
 }
 
 
 def config():
     service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
-
     if service_name:
-        # if set, with default
-        engine = engines.get(os.getenv('DATABASE_ENGINE'))
+        engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
     else:
-        # default
         engine = engines['sqlite']
-    # schema name
     name = os.getenv('DATABASE_NAME')
-
-    if not name and engine == engines['SQLITE']:
-        # sqllite is default
+    if not name and engine == engines['sqlite']:
         name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
-
     return {
         'ENGINE': engine,
         'NAME': name,
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name.upper())),
-        'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name.upper())),
+        'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
+        'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
     }
