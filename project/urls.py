3e5126cf3999088343140f258c20ef0c683c62e6
Alexander Suvorov <alexander.suvorov@agriculture.gov.ie>
2019-06-24 13:50:47 +0100

    text classification

--- project/urls.py
+++ project/urls.py
@@ -1,19 +1,20 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from rest_framework import routers
 
-from topic_modeler.views import index, health, TopicModelViewSet, TopicExtractionJobViewSet, TopicWordViewSet, \
-    TopicViewSet, TrainDataViewSet, DataRawViewSet, RunningTasksViewSet  # , train_model, extract_topic
+from topic_modeler.views import index, health, train_model, extract_topic
 
-router = routers.DefaultRouter()
-router.register(r'models', TopicModelViewSet)
-router.register(r'topics', TopicViewSet)
-router.register(r'words', TopicWordViewSet)
-router.register(r'topic_extractions', TopicExtractionJobViewSet)
-router.register(r'train_data', TrainDataViewSet)
-router.register(r'data_raw', DataRawViewSet)
-router.register(r'running_tasks', RunningTasksViewSet)
+# TopicModelViewSet, TopicExtractionJobViewSet, TopicWordViewSet, \
+#     TopicViewSet, TrainDataViewSet, DataRawViewSet, RunningTasksViewSet
+
+# router = routers.DefaultRouter()
+# router.register(r'models', TopicModelViewSet)
+# router.register(r'topics', TopicViewSet)
+# router.register(r'words', TopicWordViewSet)
+# router.register(r'topic_extractions', TopicExtractionJobViewSet)
+# router.register(r'train_data', TrainDataViewSet)
+# router.register(r'data_raw', DataRawViewSet)
+# router.register(r'running_tasks', RunningTasksViewSet)
 
 urlpatterns = [
     # test
@@ -22,10 +23,10 @@ urlpatterns = [
     # admin
     url(r'^admin/', include(admin.site.urls)),
     # REST end points
-    url(r'^rest_api/', include(router.urls)),
-    # # functionality
-    # url(r'^train_model/', train_model),
-    # url(r'^extract_topic/', extract_topic)
+    # url(r'^rest_api/', include(router.urls)),
+    # functionality
+    url(r'^train_model/', train_model),
+    url(r'^extract_topic/', extract_topic)
 ]
 
 if settings.DEBUG:
