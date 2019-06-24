ad064799cc988ff45b13bc8739ef4acd96faab90
Alexander Suvorov <alexander.suvorov@agriculture.gov.ie>
2019-06-24 13:28:37 +0100

    text classification

--- topic_modeler/views.py
+++ topic_modeler/views.py
@@ -45,28 +45,32 @@
 #     serializer_class = TopicExtractionJobSerializer
 #
 #
-# def train_model(request):
-#     # extract parameters
-#     number_of_topics = request.GET['number_of_topics']
-#     words_per_topic = request.GET['words_per_topic']
-#     # schedule job
-#     message = schedule_train_topic_model(number_of_topics, words_per_topic)
-#     # done
-#     return HttpResponse(message)
-#
-#
-# def extract_topic(request):
-#     # extract parameters
-#     text = request.GET['text']
-#     # schedule job
-#     message = do_topic_extraction(text)
-#     # done
-#     return HttpResponse(message)
+
 import os
 
 from django.http import HttpResponse
 
 from topic_modeler.models import PageView
+from topic_modeler.topic_modeler import schedule_train_topic_model, do_topic_extraction
+
+
+def train_model(request):
+    # extract parameters
+    number_of_topics = request.GET['number_of_topics']
+    words_per_topic = request.GET['words_per_topic']
+    # schedule job
+    message = schedule_train_topic_model(number_of_topics, words_per_topic)
+    # done
+    return HttpResponse(message)
+
+
+def extract_topic(request):
+    # extract parameters
+    text = request.GET['text']
+    # schedule job
+    message = do_topic_extraction(text)
+    # done
+    return HttpResponse(message)
 
 
 def index(request):
