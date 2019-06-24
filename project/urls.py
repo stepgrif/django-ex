9e903aeccebe9e8e95484ae893a0249844897854
Alexander Suvorov <alexander.suvorov@agriculture.gov.ie>
2019-06-24 12:35:21 +0100

    text classification

--- project/urls.py
+++ project/urls.py
@@ -1,19 +1,21 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from rest_framework import routers
 
-from topic_modeler.views import TopicModelViewSet, TopicExtractionJobViewSet, TopicWordViewSet, \
-    TopicViewSet, TrainDataViewSet, DataRawViewSet, RunningTasksViewSet, train_model, extract_topic, index, health
+from topic_modeler.views import index, health
 
-router = routers.DefaultRouter()
-router.register(r'models', TopicModelViewSet)
-router.register(r'topics', TopicViewSet)
-router.register(r'words', TopicWordViewSet)
-router.register(r'topic_extractions', TopicExtractionJobViewSet)
-router.register(r'train_data', TrainDataViewSet)
-router.register(r'data_raw', DataRawViewSet)
-router.register(r'running_tasks', RunningTasksViewSet)
+# TopicModelViewSet, TopicExtractionJobViewSet, TopicWordViewSet, \
+# TopicViewSet, TrainDataViewSet, DataRawViewSet, RunningTasksViewSet, train_model, extract_topic
+
+
+# router = routers.DefaultRouter()
+# router.register(r'models', TopicModelViewSet)
+# router.register(r'topics', TopicViewSet)
+# router.register(r'words', TopicWordViewSet)
+# router.register(r'topic_extractions', TopicExtractionJobViewSet)
+# router.register(r'train_data', TrainDataViewSet)
+# router.register(r'data_raw', DataRawViewSet)
+# router.register(r'running_tasks', RunningTasksViewSet)
 
 urlpatterns = [
     # test
@@ -21,11 +23,11 @@ urlpatterns = [
     url(r'^health$', health),
     # admin
     url(r'^admin/', include(admin.site.urls)),
-    # REST end points
-    url(r'^rest_api/', include(router.urls)),
-    # functionality
-    url(r'^train_model/', train_model),
-    url(r'^extract_topic/', extract_topic)
+    # # REST end points
+    # url(r'^rest_api/', include(router.urls)),
+    # # functionality
+    # url(r'^train_model/', train_model),
+    # url(r'^extract_topic/', extract_topic)
 ]
 
 if settings.DEBUG:
